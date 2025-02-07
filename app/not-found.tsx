96c1145fb6754ff076a6db2b2d4f9526f892d5b8
2MohamedHssan <mh3523976@gmail.com>
2025-02-07 13:30:52 +0200

    Fix All Error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-red-500">404</h1>
       <p className="text-lg text-gray-700">Oops! The page you are looking for does not exist.</p>
-      <a href="/" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">Go Home</a>
+      <Link href="/" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">Go Home</Link>
     </div>
   );
 }
